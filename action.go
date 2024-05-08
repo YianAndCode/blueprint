6a6d6a83b2e47d0b7d718abb2aafb28b6f8fcbc5
Y!an <i@yian.me>
2024-05-09 00:39:30 +0800

    update initBlueprint

--- action.go
+++ action.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -28,8 +30,61 @@ func initBlueprint(workDir string) error {
 		return errors.New("Reinitialized existing Blueprint repository in " + path.Join(workDir, "blueprint.json"))
 	}
 
-	// TODO:
+	cnf := Config{}
 
+	env, err := input("Environment(local/test/production, default: local): ")
+	if err != nil {
+		return err
+	}
+	if env == "" {
+		env = "local"
+	}
+	cnf.Env = env
+
+	i := 1
+	for {
+		host, _ := input(fmt.Sprintf("Input the host of DB[%d] (default: 127.0.0.1): ", i))
+		if host == "" {
+			host = "127.0.0.1"
+		}
+		portStr, _ := input(fmt.Sprintf("Input the port of DB[%d] (default: 3306): ", i))
+		port, _ := strconv.Atoi(portStr)
+		if port == 0 {
+			port = 3306
+		}
+		user, _ := input(fmt.Sprintf("Input the user of DB[%d]: ", i))
+		pass, _ := input(fmt.Sprintf("Input the pass of DB[%d]: ", i))
+		name, _ := input(fmt.Sprintf("Input the name of DB[%d]: ", i))
+		cnf.Databases = append(cnf.Databases, MySQLConfig{
+			Host: host,
+			Port: uint(port),
+			User: user,
+			Pass: pass,
+			Name: name,
+		})
+
+		more, _ := input("Add another db info? (yN): ")
+		if strings.ToLower(more) == "y" || strings.ToLower(more) == "yes" {
+			i++
+		} else {
+			break
+		}
+	}
+
+	cnfBytes, err := json.MarshalIndent(cnf, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(
+		path.Join(workDir, BlueprintConfigFileName),
+		cnfBytes,
+		0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Initialized Blueprint repository in " + path.Join(workDir, BlueprintConfigFileName))
 	return nil
 }
 
